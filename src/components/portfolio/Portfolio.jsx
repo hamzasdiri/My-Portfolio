6cfb98034c9f59dde86b73c008db9290327cf06f
hamzasdiri <sdirihamza241@gmail.com>
2023-09-02 02:22:52 +0200

    Update portfolio projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,27 +1,46 @@
-import React from 'react'
-import './portfolio.css';
-import img1 from '../../assets/baskety.png';
-import img2 from '../../assets/crud.png';
-import img3 from '../../assets/philia2.png';
-const data =[{
-  id:1,
-  image:img1,
-  title:'BASKETY',
-  github:'https://github.com/hamzasdiri/baskety',
-  demo:'https://baskety.netlify.app/'
-},
-{
-  id:2,
-  image:img2,
-  title:'CRUD APP',
-  github:'https://github.com/hamzasdiri/baskety',
-  demo:'https://baskety.netlify.app/'
-},
-{
-  id:3,
-  image:img3,
-  title:'LOGO (Mobile App)'
-}
+import React from "react";
+import "./portfolio.css";
+import img0 from "../../assets/keyme.png";
+import img1 from "../../assets/baskety.png";
+import img2 from "../../assets/watersec.png";
+import img3 from "../../assets/philia2.png";
+import img4 from "../../assets/watersecapp.jpg";
+const data = [
+  {
+    id: 1,
+    image: img0,
+    title: "KEYME",
+    github: "",
+    demo: "https://int.keyme.fr",
+  },
+  {
+    id: 2,
+    image: img4,
+    title: "WaterSec App",
+    github: "",
+    demo: "",
+  },
+  {
+    id: 3,
+    image: img2,
+    title: "CRUD APP",
+    github: "",
+    demo: "https://www.water-sec.com/",
+  },
+  {
+    id: 4,
+    image: img3,
+    title: "LOGO (Mobile App)",
+    github: "",
+    demo: "",
+  },
+  {
+    id: 5,
+    image: img1,
+    title: "BASKETY",
+    github: "https://github.com/hamzasdiri/baskety",
+    demo: "https://baskety.netlify.app/",
+  },
 ];
 
 const Portfolio = () => {
@@ -30,32 +49,42 @@ const Portfolio = () => {
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
-      <div className='container portfolio_container'>
-        {data.map(({id,image,title,github,demo}) => {
-          return(
-            <article className='portfolio_item' key={id}>
-            <div className='portfolio_item-image'>
-            <img src={image} alt={id}/>
+      <div className="container portfolio_container">
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article className="portfolio_item" key={id}>
+              <div className="portfolio_item-image">
+                <img src={image} alt={id} />
               </div>
-            <h3>{title}</h3>
-            {id <= 2 ?
-            <div className='portfolio_item-cta'>
-            <a href={github} className='btn' target='_blank' rel='noreferrer'>Github</a>
-            <a href={demo} className='btn btn-primary'  target='_blank' rel='noreferrer'>Live Demo</a>
-            </div>
-        :
-        <div className='portfolio_item-cta'></div>
-        
-        }
-          </article>
-          )
-        })
-        }
-       
-      
+              <h3>{title}</h3>
+              <div className="portfolio_item-cta">
+                {github != "" && (
+                  <a
+                    href={github}
+                    className="btn"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Github
+                  </a>
+                )}
+                {demo != "" && (
+                  <a
+                    href={demo}
+                    className="btn btn-primary"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Live Demo
+                  </a>
+                )}
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,48 +1,64 @@
-import React from 'react'
-import './services.css';
-import {BiCheck} from 'react-icons/bi';
+import React from "react";
+import "./services.css";
+import { BiCheck } from "react-icons/bi";
 const Services = () => {
   return (
     <section id="services">
       <h5>What I offer</h5>
       <h2>Services</h2>
-       <div className='container services_container center'>
-       <article className='service'>
-       <div className='service_head'>
-        <h3>SOFTWARE DEVELOPMENT</h3>
-       </div>
-       <ul className='service_list'>
-        <li>
-          <BiCheck className='service_list-icon' />
-          <p>Crafting engaging and intuitive user interfaces using HTML, CSS, and JavaScript</p>
-        </li>
-        <li>
-          <BiCheck className='service_list-icon' />
-          <p>Building robust server-side logic with technologies like Node.js and Express.js</p>
-        </li>
-        <li>
-          <BiCheck className='service_list-icon' />
-          <p>Implementing efficient databases (SQL or NoSQL) to store and manage your data</p>
-        </li>
-        <li>
-          <BiCheck className='service_list-icon' />
-          <p>Creating RESTful or GraphQL APIs for seamless communication between frontend and backend</p>
-        </li>
-        <li>
-          <BiCheck className='service_list-icon' />
-          <p>Using frameworks like Flutter to create apps that work on both iOS and Android</p>
-        </li>
-        <li>
-          <BiCheck className='service_list-icon' />
-          <p>Connecting your app to a backend server for real-time data and functionality</p>
-        </li>
-       </ul>
-       </article>
-
-       
-       </div>
+      <div className="container services_container center">
+        <article className="service">
+          <div className="service_head">
+            <h3>SOFTWARE DEVELOPMENT</h3>
+          </div>
+          <ul className="service_list">
+            <li>
+              <BiCheck className="service_list-icon" />
+              <p>
+                Crafting engaging and intuitive user interfaces using HTML, CSS,
+                and JavaScript
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon" />
+              <p>
+                Building robust server-side logic with technologies like Node.js
+                and Express.js
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon" />
+              <p>
+                Implementing efficient databases (SQL or NoSQL) to store and
+                manage your data
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon" />
+              <p>
+                Creating RESTful or GraphQL APIs for seamless communication
+                between frontend and backend
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon" />
+              <p>
+                Using frameworks like Flutter to create apps that work on both
+                iOS and Android
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service_list-icon" />
+              <p>
+                Connecting your app to a backend server for real-time data and
+                functionality
+              </p>
+            </li>
+          </ul>
+        </article>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
